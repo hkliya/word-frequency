f4b007511f9acbc8cf2967d648b6fd0aa5efeef1
Seaborn Lee <seabornlee.cn@gmail.com>
2017-03-29 21:02:46 +0800

    task1: process empty string

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -6,26 +6,10 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
-var main = require("../lib/main.js");
+var wordFrequency = require("../lib/main.js");
 
-
-describe("测试描述", function(){
-    sinon.spy(console, 'log');
-
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
-    });
-
-    it("测试用例2", function(){
-
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-
-        expect(expect_string).to.equal(result);
+describe("Word Frequency", function(){
+    it("should return empty string given empty string", function(){
+        expect(wordFrequency('')).to.equal('');
     });
 });
